e5c0b0f539e4b1e6c24001d9f22782308138733b
JsDuy <nguyenhaiduyg@gmail.com>
2025-09-16 19:17:12 +0700

    Mô tả thay đổi

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
 
 export default function Register() {
+    const inputStyle = {
+        border: "1px solid white",
+        padding: "5px",
+        margin: "20px",
+        borderRadius: "10px",
+        width: "90%",
+        backgroundColor: "#191919",
+    }
     return (
-        <div>
-            <h1>Register Now!</h1>
-            <input type="text" placeholder="User name" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Password"/>
-            <input type="text" placeholder="Password Confilm" />
-            <input type="text" placeholder="Phone" />
-            <p>You had an account? <Link href="/login">Login Now!</Link></p>
+        <div className="register">
+            <h1 className="font-bold">REGISTER NOW!</h1>
+            <input type="text" placeholder="User name" style={inputStyle}/>
+            <input type="text" placeholder="Email" style={inputStyle}/>
+            <input type="text" placeholder="Password" style={inputStyle}/>
+            <input type="text" placeholder="Password Confilm" style={inputStyle}/>
+            <input type="text" placeholder="Phone" style={inputStyle}/>
+            <p>You had an account? <Link href="/login" className="underline ask-login">Login Now!</Link></p>
         </div>
     )
 }
