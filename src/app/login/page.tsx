2901f8a34d3128203ad6ea592214915dec11da05
JsDuy <nguyenhaiduyg@gmail.com>
2025-09-16 19:31:54 +0700

    Mô tả thay đổi

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,17 @@
 export default function Login() {
+    const inputStyle = {
+        border: "1px solid white",
+        padding: "5px",
+        margin: "20px",
+        borderRadius: "10px",
+        width: "90%",
+        backgroundColor: "#191919",
+    }
     return (
-        <div>
-            <h1>Login</h1>
-            <input type="text" placeholder="User name" />
-            <input type="text" placeholder="Password" />
+        <div className="login">
+            <h1 className="font-bold">LOGIN</h1>
+            <input type="text" placeholder="User name" style={inputStyle}/>
+            <input type="text" placeholder="Password" style={inputStyle}/>
             <button type="submit">Submit</button>
         </div>
     )
